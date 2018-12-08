8ce5d1d5cf943779826ccfa6b5ff244bcf1f6052
Rongcui Dong <rongcuid@outlook.com>
2018-12-07 23:36:38 -0500

    FV written but could not test due to WSL

--- common/blinky/blinky.v
+++ common/blinky/blinky.v
@@ -8,8 +8,21 @@ module blinky
 
 reg [25:0] counter;
 always @ (posedge i_clk) 
-    counter <= counter + 1'b1;
+    counter <= counter + 26'b1;
 
 assign o_led = counter[25];
+
+`ifdef FORMAL
+    reg f_past_valid;
+    initial f_past_valid = 1'b0;
+    always @ (posedge i_clk) f_past_valid <= 1'b1;
+
+    always @ (posedge i_clk) 
+        if (f_past_valid)
+            assert(counter == $past(counter) + 26'b1);
+
+    always @ (*)
+        assert(o_led == counter[25]);
+`endif
 
 endmodule
